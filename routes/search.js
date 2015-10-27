ee8d7e5f83586c807b829363d510ba860c49dd09
U-CORP\khashayar.abdouli <khashayar.abdouli@GOT002203.corp.gds.jeppesen.com>
2015-10-27 14:30:38 +0100

    integrate search

---
+++ routes/search.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+// var nano = require('nano')('http://admin:nobodyishere@localhost:5984');
+// var database = nano.db.use('test');
+
+/*
+database.update = function(obj, key, callback) {
+	var db = this;
+	db.get(key, function (error, existing) { 
+		if(!error) 
+			obj._rev = existing._rev;
+		db.insert(obj, key, callback);
+	});
+}
+*/
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('search', { title: 'Meal Jungle Cpanel' });
+});
+
+
+module.exports = router;
