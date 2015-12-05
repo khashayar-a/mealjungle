36dc4c06ba10b7903a901478482f1eaa77ec591e
Alexander <aliaksandr12@yahoo.com>
2015-12-05 14:57:14 +0100

    5Dec 2015
    modal added

--- public/javascripts/restaurant.js
+++ public/javascripts/restaurant.js
@@ -1,3 +1,13 @@
-    $(document).on('click', '.item-box-name', function(){
-        
+jQuery(document).ready(function() {
+    jQuery('.tabs .tab-links div').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('link');
+
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
     });
+});
