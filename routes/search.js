0434a1a6db5c84d6aab4941eedd2fc85d1d97f27
khashayar.abd <khashayar.abd@medopad.com>
2015-12-13 16:17:37 +0100

    missing library

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var Q = require('q');
 
 var elasticsearch = require('elasticsearch');
 var client = new elasticsearch.Client({
